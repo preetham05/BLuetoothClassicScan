208369ef55003f305ee4930a53d4e4ef9057ef41
Agent Bluetooth <preetham.kannan@zigpos.com>
2023-08-11 10:29:58 +0200

    added timeouts

--- BTScanner.py
+++ BTScanner.py
@@ -8,14 +8,16 @@ import logging
 import bluetooth
 import uuid
 import re
+import sys
+import os
 import socket
-
 
 baseURL='http://zigpos-gateway331/rtls_api/rest'
 
 BT_Device = {}
 ScanData = []
 address_base = 0x70B3
+retry_count = 0
 anchor_address = hex(address_base<<48 | (uuid.getnode()))
 print('HexValue:'+ anchor_address)
 anchor_address = int(anchor_address,16)
@@ -24,6 +26,7 @@ logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
     level=logging.INFO,
     datefmt='%Y-%m-%d %H:%M:%S')
+
 class MyDiscoverer(bluetooth.DeviceDiscoverer):
 
     def pre_inquiry(self):
@@ -32,37 +35,7 @@ class MyDiscoverer(bluetooth.DeviceDiscoverer):
     def device_discovered(self, address, device_class, rssi, name):
         # print("{} - {}".format(address, name))
         address = address.replace(":", "")
-        # print("{} - {}".format(address, name))
 
-        # get some information out of the device class and display it.
-        # voodoo magic specified at:
-        # https://www.bluetooth.org/foundry/assignnumb/document/baseband
-        # major_classes = ("Miscellaneous",
-        #                  "Computer",
-        #                  "Phone",
-        #                  "LAN/Network Access Point",
-        #                  "Audio/Video",
-        #                  "Peripheral",
-        #                  "Imaging")
-        # major_class = (device_class >> 8) & 0xf
-        # if major_class < 7:
-        #     print(" " + major_classes[major_class])
-        # else:
-        #     print("  Uncategorized")
-
-        # print("  Services:")
-        # service_classes = ((16, "positioning"),
-        #                    (17, "networking"),
-        #                    (18, "rendering"),
-        #                    (19, "capturing"),
-        #                    (20, "object transfer"),
-        #                    (21, "audio"),
-        #                    (22, "telephony"),
-        #                    (23, "information"))
-
-        # for bitpos, classname in service_classes:
-        #     if device_class & (1 << (bitpos-1)):
-        #         print("   ", classname)
         BT_Device[address]=rssi
         for bt_macAdreess, rssi in BT_Device.items():
 
@@ -86,9 +59,15 @@ class MyDiscoverer(bluetooth.DeviceDiscoverer):
             response_message = r.content
             if response_code == 200:
                 logging.info("The address %s sent the beacon", address)
+                retry_count = 0
 
             else:
                 logging.warning("The address %s failed to send the beacon", address)
+                retry_count+= 1
+                if retry_count == 10:
+                    time.sleep(5)
+                    os.execl(sys.executable, sys.executable, *sys.argv)
+
     def inquiry_complete(self):
         logging.warning("Scanning completed")
 
@@ -112,14 +91,18 @@ print(r.text)
 response_code = r.status_code
 logging.warning(response_code)
 response_message = r.content
-if response_code == 200:
+if response_code == 200 or response_code == 204:
     logging.warning("The address %s added to the devices ", anchor_address)
 
-else:
+elif response_code == 404:
     logging.warning("The address %s failed to be added to the devices ", anchor_address)
+    time.sleep(5)
+    os.execl(sys.executable, sys.executable, *sys.argv)
+
 
 
 d = MyDiscoverer()
+
 d.find_devices(lookup_names=True)
 
 readfiles = [d, ]
@@ -131,4 +114,10 @@ while True:
         d.process_event()
     if d.done:
         time.sleep(0.02)
-        d.find_devices(lookup_names=True)
+        try:
+            d.find_devices(lookup_names=True)
+        except:
+            pass
+        time.sleep(15)
+
+
